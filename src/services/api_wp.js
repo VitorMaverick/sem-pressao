c536e312ae09caf9c766dadd52fbc217ebf0479b
VitorMaverick <mr.maverick1@gmail.com>
2020-10-13 01:14:22 -0300

    commit

--- src/services/api_wp.js
+++ src/services/api_wp.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api_wp = axios.create({
-    baseURL: 'https://sem-pressao.000webhostapp.com/',
+    baseURL: 'https://sem-pressao.000webhostapp.com/wp-json/wp/v2',
 
 });
 export default api_wp;
