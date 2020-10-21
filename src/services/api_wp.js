d2cd13c0bc0d19f561a1c8f43486833d0f25d2fb
VitorMaverick <mr.maverick1@gmail.com>
2020-10-21 20:21:16 -0300

    commit https

--- src/services/api_wp.js
+++ src/services/api_wp.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api_wp = axios.create({
-    baseURL: 'http://shrouded-cliffs-29775.herokuapp.com/wp-json/wp/v2',
+    baseURL: 'https://shrouded-cliffs-29775.herokuapp.com/wp-json/wp/v2',
 
 });
 export default api_wp;
